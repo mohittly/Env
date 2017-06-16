e47840ceca46b44224c611214d1df8b0b0409a95
mohittly <mohittly@gmail.com>
2017-06-16 23:23:56 +0530

    materializecss files changed.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
 var first_name = document.getElementById('first_name');
 var last_name = document.getElementById('last_name');
-var email = document.getElementById('email');
-var contact = document.getElementById('contact');
+var icon_telephone = document.getElementById('icon_telephone');
 var age = document.getElementById('age');
-var gender = document.getElementById('gender');
-var test5 = document.getElementById('test5');
-var test6 = document.getElementById('test6');
-var test7 = document.getElementById('test7');
-var textarea1 = document.getElementById('textarea1');
+var from = document.getElementById('from');
+var to = document.getElementById('to');
 var submit = document.getElementById('submit');
+var stud = document.getElementById('stud');
 var x;
+var gen = document.getElementById('gender');
 var firebaseStudRef = firebase.database().ref().child("i");
 
 firebaseStudRef.on('value',function(datasnapshot){
@@ -22,34 +20,18 @@ x++;
 var firebaseRef = firebase.database().ref();
 var fname = first_name.value;
 var lname = last_name.value;
-var tel = contact.value;
-var em = email.value;
-var ag = age.value;var t5,t6,t7;
-if(test5.checked)
-{ t5="Yes"; }
-else {t5="No";}
-if(test6.checked)
-{ t6="Yes"; }
-else {t6="No";}
-if(test7.checked)
-{ t7="Yes"; }
-else {t7="No";}
-var t = textarea1.value;
-var g = gender.value;
-alert("Thank you for applying. We will reach out to you soon");
-location.reload();
+var tel = icon_telephone.value;
+var ag = age.value;
+var fro = from.value;
+var t = to.value;
+var g = gen.value;
 firebaseRef.child("i").set(x);
 firebaseRef.child("user "+x).child("first name").set(fname);
 firebaseRef.child("user "+x).child("last name").set(lname);
 firebaseRef.child("user "+x).child("contact").set(tel);
 firebaseRef.child("user "+x).child("age").set(ag);
-firebaseRef.child("user "+x).child("email").set(em);
-firebaseRef.child("user "+x).child("Wedding").set(t5);
-firebaseRef.child("user "+x).child("Competition").set(t6);
-firebaseRef.child("user "+x).child("Learning").set(t7);
-firebaseRef.child("user "+x).child("Message").set(t);
+firebaseRef.child("user "+x).child("start date").set(fro);
+firebaseRef.child("user "+x).child("end date").set(t);
 firebaseRef.child("user "+x).child("gender").set(g);
-
-
 
 }
